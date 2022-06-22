d538d231ef5b898607e00116a573cd9ad7ac5b2c
Вадим Савитский <vadimsavitskij@MacBook-Pro-Vadim.local>
2022-06-22 15:31:16 +0300

    Initial commit

---
+++ src/main/java/com/example/gb_projecting_app/GbProjectingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.gb_projecting_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbProjectingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbProjectingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gb_projecting_app/GbProjectingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gb_projecting_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbProjectingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
